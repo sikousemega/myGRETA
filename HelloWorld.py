72b6299b46049027d22615a9a073f318728305db
SIKOU SEMEGA <95615364+sikousemega@users.noreply.github.com>
2021-12-06 15:40:16 +0100

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -3,6 +3,5 @@ str = "!hello world"
 print("\n" + str)
 
 str = "smodif sur la new bran"
-str = " Racontez tout sur python"
-st =" nouvelle branche"
-print("\n" + str)
+strnew = ""
+strnew="Hello Samia"
